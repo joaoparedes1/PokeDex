1a01886d2ba449c49cf6e1e9632fa04dbb946221
Aluno <aluno>
2022-09-22 21:38:10 -0300

    Aula 22/09

---
+++ app/src/main/java/br/com/up/pokedex/adapter/PokeAdapter.kt
@@ -0,0 +1,14 @@
+package br.com.up.pokedex.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PokeAdapter {
+
+
+
+
+    inner class PokeViewHolder(itemView:View) :
+        RecyclerView.ViewHolder(itemView)
+
+}
