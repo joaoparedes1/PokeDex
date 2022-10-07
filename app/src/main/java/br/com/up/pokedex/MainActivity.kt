43e4548b60120d265ab20abcacee132508411239
Aluno <aluno>
2022-10-06 22:15:34 -0300

    aula 06/10

--- app/src/main/java/br/com/up/pokedex/MainActivity.kt
+++ app/src/main/java/br/com/up/pokedex/MainActivity.kt
@@ -2,12 +2,24 @@ package br.com.up.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.com.up.pokedex.adapter.PokeAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView : RecyclerView =
+            findViewById(R.id.recycler_pokemons)
+
+        recyclerView.layoutManager =
+            GridLayoutManager(this, 2)
+
+        recyclerView.adapter = PokeAdapter()
+
     }
 
 }
--- app/src/main/java/br/com/up/pokedex/adapter/PokeAdapter.kt
+++ app/src/main/java/br/com/up/pokedex/adapter/PokeAdapter.kt
@@ -1,14 +1,41 @@
 package br.com.up.pokedex.adapter
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import br.com.up.pokedex.R
+import com.squareup.picasso.Picasso
 
-class PokeAdapter {
-
-
-
+class PokeAdapter() :
+    RecyclerView.Adapter
+    <PokeAdapter.PokeViewHolder>() {
 
     inner class PokeViewHolder(itemView:View) :
         RecyclerView.ViewHolder(itemView)
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokeViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        val layout = inflater
+            .inflate(R.layout.view_poke_item,
+                                    parent,
+                                    false)
+
+        return PokeViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: PokeViewHolder, position: Int) {
+
+        val url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png"
+
+        val imageView : ImageView = holder.itemView.findViewById(R.id.image_pokemon)
+
+        Picasso.get().load(url).into(imageView)
+    }
+
+    override fun getItemCount(): Int {
+        return 10
+    }
 }
